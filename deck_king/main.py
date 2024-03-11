564ad56445aabf2a9ce55e1b8d66aa2e3b3fad1a
SonicHaveJar <javier.sancho.olano@gmail.com>
2024-03-11 20:16:51 +0100

    Docker launch

--- deck_king/main.py
+++ deck_king/main.py
@@ -3,12 +3,24 @@ from .db import Base, engine
 from .routers import user
 from .routers import websockets
 
+from fastapi.middleware.cors import CORSMiddleware
+
 # https://fastapi.tiangolo.com/tutorial/
 # https://fastapi.tiangolo.com/python-types/
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(websockets.router)
